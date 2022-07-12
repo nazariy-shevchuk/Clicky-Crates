9998310bd93541a085ba23668f3f25a6d737c624
nazariy-shevchuk <nazariy.shevchuk00@gmail.com>
2022-07-12 15:02:11 +0300

    pause menu

---
+++ Assets/Scripts/PauseControl.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseControl : MonoBehaviour
+{
+    public static bool gameIsPaused;
+    public GameObject pauseScreen;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameIsPaused = !gameIsPaused;
+            PauseGame();
+        }
+    }
+
+    void PauseGame()
+    {
+        if (gameIsPaused)
+        {
+            Time.timeScale = 0f;
+            pauseScreen.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            pauseScreen.SetActive(false);
+        }
+    }
+}
